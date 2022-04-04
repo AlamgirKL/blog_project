ef6777388c2f1b632b377b3d2a6090b13268e989
AlamgirKL <alamgirkair452@gmil.com>
2022-04-04 16:12:58 +0600

    blog_project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,146 @@
+@extends('layouts.app')
+
+<style>
+    /* .background-image {
+
+    
+        background: url('../img/bc.jpg' alt="gfhgdt");
+        
+        /* background: url('https://cdn.pixabay.com/photo/2020/04/30/15/34/code-5113374_960_720.jpg');
+     */
+} */
+
+</style>
+<!-- <link rel="stylesheet" href="/css/app.css"> -->
+
+@section('content')
+<div class="background-image grid grid-cols-1 m-auto">
+    <div class=" flex text-gray-100 pt-10">
+        <div class=" m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center" >
+            <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
+                Do You Wante to Become  A  Devoloper ? 
+
+            </h1>
+            <a 
+                
+                href="/blog"
+                class=" text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
+            Rede More</a>
+
+        </div>
+
+    </div>
+    
+
+</div>
+<div>
+       
+
+    </div>
+
+
+    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+        <div>
+        <img src="../images/abc.jpg" alt="blog" width ="700" >
+
+        </div>
+        
+        
+
+        <div class="m-auto sm:m-auto text-laft w-4/5 block">
+                <h2 class="text-4xl font-extrabold text-gray-600">
+                String to be a better web deboloper ? 
+
+                 </h2>
+            <p class=" py-8 text-gray-500 text-xl">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+             Modi repellendus, consectetur sequi obcaecati amet aut sit qu
+             
+
+            </p>
+            <p class=" py-8 text-gray-600 text-l pb-9 ">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt et, delectus natus molestiae quo sunt earum repellendus officiis ex quisquam hic tenetur! Ducimus itaque officiis, tempora quia minima obcaecati voluptatibus.
+
+
+            </p>
+            <a href="/blog"
+         class="uppercase bg-blue-500 text-gray-100 text-s font-extrabold py-3 px-8 rounded-3xl">
+         Find Out More 
+         </a>
+     </div>
+    
+     
+    </div>
+    
+    <div class=" text-center p-15  bg-black text-white "> 
+        
+
+        <h2 class= " pb-5  text-l text-2xl ">
+            i'm an expert in..
+        </h2>
+        <span class="font-extrabold block text-3xl py-1">
+            Ui Design 
+
+        </span>
+        <span class="font-extrabold block text-3xl py-1">
+            Web Design 
+
+        </span>
+        
+        <span class="font-extrabold block text-3xl py-1">
+            Digital Markating 
+
+        </span>
+        <span class="font-extrabold block text-3xl py-1">
+            Web Development
+
+        </span>
+
+    </div>
+
+    <div class="text-center py-15">
+        <span class=" uppercase text-sm text-cool-gray-400">
+                BLOG
+
+        </span>
+        <h2 class=" text-3xl font-bold py-10 ">
+            Recent Post
+
+        </h2>
+        <span class=" m-auto w-4/5 text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam repudiandae blanditiis vitae illo, porro eum est sed recusandae, harum pariatur qui! Repudiandae nobis sunt inventore debitis, temporibus odit eaque eligendi!
+        </span>
+    </div>
+
+    <div class=" sm:grid grid-cols-2 w-4/5 m-auto">
+        <div class=" flex bg-yellow-700 text-gray-100 pt-10">
+            <div class ="m-auto pt-4 pb-16 sm:m-auto w-4/5 block">
+                <span class ="uppercase text-xs">
+                    PHP  
+
+                </span>
+
+                <h3 class=" text-xs font-bold p-10 ">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad amet quo sunt, 
+                    labore est iusto quaerat dignissimos optio mollitia sapiente ex reiciendis veniam velit recusandae quos vitae fugit aut!  
+
+                </h3>
+
+                <a 
+                href="" 
+                class="uppercase bg-transparent border-2 border-gray-100 text-gray-100 text-xs font-extrabold py-3 px-5  rounded-3xl">
+                Find Out More
+                </a>
+
+
+                
+            </div>
+
+        </div>
+        <div>
+            <div><img src="../images/abc.jpg" alt="blog" width ="700" ></div>
+        </div>
+    </div>
+
+
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,109 @@
+<footer class="bg-gray-800 py-20 mt-20">
+    <div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto border-b-2 border-gray-700">
+        <div class=" mr-8">
+            <h3 class=" text-1 sm:font-bold text-gray-100">
+                page
+
+            </h3>
+            <ul class=" py-4 sm:text-5 pt-4 text-gray-400" >
+            <li class="pb-1">
+                    <a href="/"></a>
+                    home
+
+                </li> 
+                <li class="pb-1">
+                    <a href="/blog"></a>
+                    Blog
+
+                </li> 
+                <li class="pb-1">
+                    <a href="/login"></a>
+                    login
+
+                </li> 
+
+                <li class="pb-1">
+                    <a href="/register"></a>
+                    Registion
+
+                </li> 
+
+
+            </ul>
+
+        </div>
+
+        <div  class="  m-auto">
+            <h3 class=" text-1 sm:font-bold text-gray-100">
+                Find US
+
+            </h3>
+            <ul class=" py-4 sm:text-5 pt-5 text-gray-400" >
+            <li class="pb-1">
+                    <a href="/"></a>
+                    Location
+
+                </li> 
+                <li class="pb-1">
+                    <a href="/"></a>
+                    Blog
+
+                </li> 
+                <li class="pb-1">
+                    <a href="/"></a>
+                    About
+
+                </li> 
+
+                <li class="pb-1">
+                    <a href="/"></a>
+                    Home
+
+                </li> 
+
+
+            </ul>
+
+        </div>
+        <div class=" m m-auto">
+            <h3 class=" text-1 sm:font-bold text-gray-100">
+               contact
+
+            </h3>
+            <ul class=" py-4 sm:text-5 pt-4 text-gray-400" >
+            <li class="pb-1">
+                    <a href="/"></a>
+                    Adress
+
+                </li> 
+                <li class="pb-1">
+                    <a href="/"></a>
+                    Phone
+
+                </li> 
+                <li class="pb-1">
+                    <a href="/"></a>
+                    Email
+
+                </li> 
+
+                <li class="pb-1">
+                    <a href="/"></a>
+                    media
+
+                </li> 
+
+
+            </ul>
+
+        </div>
+
+        
+        
+    </div>
+    <p class=" w-25 w-4/5 pb-3 m-auto text-xs text-gray-100 pt-2">
+        Copyright 2022 All Right  ALamgir kabir
+
+    </p>
+</footer>
+
